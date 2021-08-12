e9f1225cda6650a79c55b3e49dc6c613789da96f
mlondong <mauricio.londono@gmail.com>
2021-08-11 22:02:49 -0500

    implemented lombok

--- src/main/java/co/com/foodbank/vault/dto/VaultData.java
+++ src/main/java/co/com/foodbank/vault/dto/VaultData.java
@@ -2,9 +2,17 @@ package co.com.foodbank.vault.dto;
 
 import java.util.Collection;
 import co.com.foodbank.address.dto.Address;
-import co.com.foodbank.address.dto.IAddress;
 import co.com.foodbank.contribution.state.ContributionData;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/**
+ * @author mauricio.londono@gmail.com co.com.foodbank.vault.dto 11/08/2021
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VaultData {
 
     private String id;
@@ -16,61 +24,6 @@ public class VaultData {
     private String contact;
 
     private Collection<ContributionData> contribution;
-
-
-    public VaultData() {
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-
-    public VaultData(Address address, String phones, String contact,
-            Collection<ContributionData> contribution) {
-        this.address = address;
-        this.phones = phones;
-        this.contact = contact;
-        this.contribution = contribution;
-    }
-
-    public IAddress getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhones() {
-        return phones;
-    }
-
-    public void setPhones(String phones) {
-        this.phones = phones;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public Collection<ContributionData> getContribution() {
-        return contribution;
-    }
-
-    public void setContribution(Collection<ContributionData> contribution) {
-        this.contribution = contribution;
-    }
 
 
 
